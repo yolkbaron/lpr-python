b0812d122423933d90522b0b04e34d5659458307
yolkbaron <71354158+yolkbaron@users.noreply.github.com>
2020-09-16 21:40:52 +0300

    Add files via upload

---
+++ lab3/05-idealgas.py
@@ -0,0 +1,46 @@
+from random import *
+from turtle import *
+
+
+number_of_atoms = 10
+steps_of_time_number = 2000
+size = 100
+atomsize = 5
+
+penup()
+goto(size, size)
+pendown()
+goto(size, -size)
+goto(-size, -size)
+goto(-size, size)
+goto(size, size)
+penup()
+ht()
+gas = [Turtle(shape='circle') for i in range(number_of_atoms)]
+x = {}
+y = {}
+vx = {}
+vy = {}
+for atom in gas:
+    atom.penup()
+    atom.speed(0)
+    atom.left(90)
+    x[atom] = randint(-size + atomsize, size - atomsize)
+    y[atom] = randint(-size + atomsize, size - atomsize)
+    vx[atom] = randint(-size, size)/10
+    vy[atom] = randint(-size, size)/10
+    atom.shapesize(atomsize/10)
+    atom.goto(x[atom], y[atom])
+
+for i in range(steps_of_time_number):
+    for atom in gas:
+        x[atom] += vx[atom]
+        y[atom] += vy[atom]
+        if x[atom] > size - atomsize or x[atom] < -size + atomsize:
+            x[atom] -= vx[atom]
+            vx[atom] = -vx[atom]
+        if y[atom] > size - atomsize or y[atom] < -size + atomsize:
+            y[atom] -= vy[atom]
+            vy[atom] = -vy[atom]
+        atom.goto(x[atom], y[atom])
+done()
